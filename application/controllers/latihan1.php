817a8ae50c353a998883127be08a935355afe50b
iam-IAMmaulana <iammaulana447@gamil.com>
2023-03-19 00:02:04 +0700

    welcome

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,10 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan </h1>";
+        echo "Nama saya ilham maulana wijaya
+                saya tinggal di sawangan lama ,depok";
+    }
+}
